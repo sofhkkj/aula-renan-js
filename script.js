16371396adb4e77a7e9cbbc3862559ae4a2b3232
sof <130997583+sofhkkj@users.noreply.github.com>
2023-05-11 10:14:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function função() {
+    alert ("O pato, vinha cantando alegremente quenquenquen, quando o marreco sorridente pediu, para entrar também no samba, no samba, no samba. O ganso (tududum) gostou da dupla e fez tambem quenquenquen...")
+}
